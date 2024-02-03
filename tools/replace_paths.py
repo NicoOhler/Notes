14302a2f4e17364a84c5c46c65da7ff0d7f3c846
NicoOhler <nico.ohler@gmail.com>
2024-02-03 16:18:15 +0100

    vault backup: 2024-02-03 16:18:15
    Affected files:
    .obsidian/workspace.json
    tools/replace_paths.py
    tools/test/a.md

--- tools/replace_paths.py
+++ tools/replace_paths.py
@@ -1,1 +1,32 @@
-# replace paths like [[file.ext]] with [[relative/path/to/file.ext]] in all markdown files in the current directory and subdirectories
+# replaces paths like [[file]] with [[relative/path/to/file.ext]] in all markdown files of a given directory and its subdirectories
+
+import os
+import re
+import glob
+import sys
+
+directory
+directory = sys.argv[1]
+if not os.path.isdir(directory):
+    print("The given directory does not exist.")
+    sys.exit(1)
+
+
+# get all files in the current directory and its subdirectories
+files = glob.glob(directory, recursive=True)
+
+# iterate over all markdown files
+for file_path in files:
+    # open the file and read the contents
+    with open(file_path, "r") as file:
+        contents = file.read()
+
+    # replace all paths with the relative path
+    relative_path = os.path.relpath(file_path, os.getcwd())
+    new_contents = re.sub(
+        r"\[\[(.*?)\]\]", r"[[{}/\1]]".format(relative_path), contents
+    )
+
+    # write the new contents to the file
+    with open(file_path, "w") as file:
+        file.write(new_contents)
