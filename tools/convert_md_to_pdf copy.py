2ca722eb0784b37283d109807d8abf174f5d8970
NicoOhler <nico.ohler@gmail.com>
2024-02-04 18:04:02 +0100

    vault backup: 2024-02-04 18:04:01
    Affected files:
    .obsidian/workspace.json
    Informatik/output.pdf
    head.tex
    tools/convert_md_to_pdf copy.py
    tools/convert_md_to_pdf.py
    tools/head.tex

---
+++ tools/convert_md_to_pdf copy.py
@@ -0,0 +1,24 @@
+import requests
+
+API_KEY = "814babe0220c02f68e4245aa40e83b64751ec29587af5592f606c0ca304614ed"
+ENDPOINT = "https://127.0.0.1:27124"
+
+response = requests.get(
+    ENDPOINT + "/commands/",
+    headers={"accept": "text/markdown", "Authorization": "Bearer " + API_KEY},
+    verify=False,  # disable SSL verification
+)
+
+# print(response.text)
+
+# Obsidian Git: List changed files
+# command-palette:open
+# obsidian-git:list-changed-files
+response = requests.put(
+    ENDPOINT + "/commands/" + "editor:save-file",
+    ENDPOINT + "/commands/" + "editor:save-file",
+    headers={"accept": "*/*", "Authorization": "Bearer " + API_KEY},
+    verify=False,  # disable SSL verification
+)
+
+print(response.text)
