3da4917ec11c6ece785c75c2860d1ec89a571307
NicoOhler <nico.ohler@gmail.com>
2024-02-04 13:28:25 +0100

    vault backup: 2024-02-04 13:28:25
    Affected files:
    tools/convert_md_to_pdf.py

--- tools/convert_md_to_pdf.py
+++ tools/convert_md_to_pdf.py
@@ -2,16 +2,20 @@ import requests
 
 API_KEY = "814babe0220c02f68e4245aa40e83b64751ec29587af5592f606c0ca304614ed"
 ENDPOINT = "https://127.0.0.1:27124"
-"""
-curl -X 'GET' \
-  'https://127.0.0.1:27124/active/' \
-  -H 'accept: text/markdown' \
-  -H 'Authorization: Bearer 814babe0220c02f68e4245aa40e83b64751ec29587af5592f606c0ca304614ed'
-"""
 
 response = requests.get(
-    ENDPOINT + "/active/",
+    ENDPOINT + "/commands/",
     headers={"accept": "text/markdown", "Authorization": "Bearer " + API_KEY},
+    verify=False,  # disable SSL verification
 )
 
 print(response.text)
+
+
+response = requests.put(
+    ENDPOINT + "/commands/",
+    headers={"accept": "text/markdown", "Authorization": "Bearer " + API_KEY},
+    verify=False,  # disable SSL verification
+    json={
+        "command": "list-changed-files",
+        "repo": "
