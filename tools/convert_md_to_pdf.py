74a6408483c0aae5177999ea7021f288f4af8a79
NicoOhler <nico.ohler@gmail.com>
2024-02-04 18:08:59 +0100

    vault backup: 2024-02-04 18:08:59
    Affected files:
    tools/convert_md_to_pdf.py

--- tools/convert_md_to_pdf.py
+++ tools/convert_md_to_pdf.py
@@ -1,3 +1,5 @@
-"""
-pandoc -H ../tools/head.tex --pdf-engine=pdflatex OOP2.md -o output.pdf -s -V geometry:"top=2cm, bottom=1.5cm, left=2cm, right=2cm
-"""
+# sudo apt install texlive-latex-recommended
+# sudo apt install pandoc
+import os
+
+pandoc = 'pandoc -H ../tools/head.tex --pdf-engine=pdflatex FILENAME.md -o FILENAME.pdf --s -V geometry:"top=2cm, bottom=1.5cm, left=2cm, right=2cm"'
