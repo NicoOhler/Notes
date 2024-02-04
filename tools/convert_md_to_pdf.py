1e6d970de750c291cdd2657d7b9908de08dedc5e
NicoOhler <nico.ohler@gmail.com>
2024-02-04 18:19:02 +0100

    vault backup: 2024-02-04 18:18:59
    Affected files:
    .obsidian/snippets/obsidian-snippets-main/README.pdf
    .obsidian/snippets/obsidian-snippets-main/images/README.pdf
    tools/convert_md_to_pdf.py

--- tools/convert_md_to_pdf.py
+++ tools/convert_md_to_pdf.py
@@ -9,11 +9,11 @@ def get_md_files(path):
         for file in f:
             file_extension = os.path.splitext(file)[1]
             if file_extension == ".md":
-                md_files.append(os.path.join(r, file))
+                md_files.append(os.path.join(r, file)[:-3])  # remove .md
     return md_files
 
 
-pandoc = 'pandoc -H ../tools/head.tex --pdf-engine=pdflatex FILENAME.md -o FILENAME.pdf --s -V geometry:"top=2cm, bottom=1.5cm, left=2cm, right=2cm"'
+pandoc = 'pandoc -H ../tools/head.tex --pdf-engine=pdflatex FILENAME.md -o FILENAME.pdf -s -V geometry:"top=2cm, bottom=1.5cm, left=2cm, right=2cm"'
 path = "../"
 md_files = get_md_files(path)
 for md_file in md_files:
