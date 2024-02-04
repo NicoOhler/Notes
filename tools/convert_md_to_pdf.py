fdf6c1724e7d3658d0f7f64a516c4c6f891030fc
NicoOhler <nico.ohler@gmail.com>
2024-02-04 13:23:25 +0100

    vault backup: 2024-02-04 13:23:25
    Affected files:
    tools/convert_md_to_pdf.py

--- tools/convert_md_to_pdf.py
+++ tools/convert_md_to_pdf.py
@@ -1,3 +1,17 @@
-import pyomd
+import requests
 
-pyomd.convert_md_to_pdf("example.md", "example.pdf")
+API_KEY = "814babe0220c02f68e4245aa40e83b64751ec29587af5592f606c0ca304614ed"
+ENDPOINT = "https://127.0.0.1:27124"
+"""
+curl -X 'GET' \
+  'https://127.0.0.1:27124/active/' \
+  -H 'accept: text/markdown' \
+  -H 'Authorization: Bearer 814babe0220c02f68e4245aa40e83b64751ec29587af5592f606c0ca304614ed'
+"""
+
+response = requests.get(
+    ENDPOINT + "/active/",
+    headers={"accept": "text/markdown", "Authorization": "Bearer " + API_KEY},
+)
+
+print(response.text)
